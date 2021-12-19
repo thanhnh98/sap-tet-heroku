4f483c79dfea5536967e99fbe5c474452dc34ded
thanh <thanh150898@gmail.com>
2021-12-19 16:42:15 +0700

    init testing for chatting

--- src/Application.kt
+++ src/Application.kt
@@ -2,10 +2,14 @@ package com.tlife
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.websocket.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
@@ -14,6 +18,31 @@ fun main() {
 }
 @Suppress("unused") // Referenced in application.conf
 fun Application.module(testing: Boolean = false) {
+    install(WebSockets)
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") {
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
+            }
+        }
+    }
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
@@ -23,4 +52,5 @@ fun Application.module(testing: Boolean = false) {
             call.respond("Hello 4, test server deployment")
         }
     }
+
 }
