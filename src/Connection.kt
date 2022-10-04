baae28ae21c5e58d5fe16c51bf79c21923329124
Thanh <h.thanhnguyen@okxe.com>
2022-10-04 12:30:13 +0700

    remove model connection

--- src/Connection.kt
+++
@@ -1,7 +0,0 @@
-package com.tlife
-
-import com.tlife.model.RoomModel
-import io.ktor.websocket.*
-import java.util.concurrent.atomic.AtomicInteger
-
-data class Connection(val session: DefaultWebSocketServerSession)
