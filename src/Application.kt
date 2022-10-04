0bdeb041f1a10598764272f995c946a9d91929d1
Thanh <h.thanhnguyen@okxe.com>
2022-10-04 14:32:32 +0700

    for testing inapp update

--- src/Application.kt
+++ src/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.sessions.*
 import io.ktor.websocket.*
 
 fun main() {
-    val isDev = false
+    val isDev = true
     embeddedServer(Netty, port = 8080, if (isDev) "127.0.0.1" else "192.168.0.97") {
         module()
     }.start(wait = true)
@@ -19,14 +19,41 @@ fun main() {
 fun Application.module(testing: Boolean = false) {
     install(WebSockets)
     install(Sessions)
-
+    var appVersionModel = AppVersionModel(
+        324,
+        "3.2.4",
+        false
+    )
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/version") {
-            call.respondText("in side version", contentType = ContentType.Text.Plain)
+        get("/app-config") {
+            call.respondText(
+                Version(appVersionModel).toJson(),
+                contentType = ContentType.Text.Plain)
+        }
+
+        get("/app-config/{code}") {
+            val versioncode = this.call.request.call.parameters["code"]?.toInt()?:0
+
+            appVersionModel = appVersionModel.copy(
+                lastVersionCode = versioncode
+            )
+            call.respondText(
+                Version(appVersionModel).toJson(),
+                contentType = ContentType.Text.Plain)
         }
     }
 }
+
+data class AppVersionModel(
+    val lastVersionCode: Int,
+    val lastVersionName: String,
+    val forceUpdate: Boolean
+)
+
+data class Version(
+    val version: AppVersionModel
+)
